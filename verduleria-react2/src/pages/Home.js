ac56df9b3af847656e409079552657c4d6f9b8f0
Stefano Lista <listafefo@gmail.com>
2022-09-11 18:47:21 -0300

    test

--- verduleria-react2/src/pages/Home.js
+++ verduleria-react2/src/pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div className="container">
             <section className='home-container'>
-              <h2 className="homeheader">La verduleria de tu barrio</h2>
+              <h2 className="homeheader">La verduleria de tu barrio!</h2>
               <p className="hometext">Bienvenido a la nueva tienda de la verduleria que mas queres!</p>
               <p className="hometext">Aca vas a encontrar nuestros productos que tanto te gustan, frescos, naturales y VIRTUALES</p>
               <p className="hometext">Gracias por elegirnos</p>
